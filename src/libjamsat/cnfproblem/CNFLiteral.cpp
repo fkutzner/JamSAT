876148f01fd19b28940bf8066b08bbc6e16b2a01
Felix Kutzner <felixkutzner@gmail.com>
2017-06-17 14:39:57 +0200

    clang-formatted the entire project

--- src/libjamsat/cnfproblem/CNFLiteral.cpp
+++ src/libjamsat/cnfproblem/CNFLiteral.cpp
@@ -1,14 +1,14 @@
 #include "CNFLiteral.h"
 
 namespace jamsat {
-    std::ostream& operator<< (std::ostream& stream, const CNFVar& variable) {
-        stream << variable.getRawValue();
-        return stream;
-    }
-    
-    std::ostream& operator<< (std::ostream& stream, const CNFLit& literal) {
-        stream << (literal.getSign() == CNFSign::POSITIVE ? "+" : "-");
-        stream << literal.getVariable();
-        return stream;
-    }
+std::ostream &operator<<(std::ostream &stream, const CNFVar &variable) {
+  stream << variable.getRawValue();
+  return stream;
+}
+
+std::ostream &operator<<(std::ostream &stream, const CNFLit &literal) {
+  stream << (literal.getSign() == CNFSign::POSITIVE ? "+" : "-");
+  stream << literal.getVariable();
+  return stream;
+}
 }
