9f8c023939a7b889dfb1c9953e4a662de025461a
Felix Kutzner <felixkutzner@gmail.com>
2017-07-04 21:33:53 +0200

    fixed clang sanitizer usage

--- etc/TravisBuild.sh
+++ etc/TravisBuild.sh
@@ -4,20 +4,7 @@ set -e
 # For this script, it's assumed that the current working directory is an empty
 # directory where the build files can be placed.
 
-if [ "${SONARSOURCE_SCAN}" != "1" ]
-then
-  echo "Building JamSAT in ${JAMSAT_MODE} mode."
-
-  if [ "${JAMSAT_MODE}" = "COVERAGE" ]
-  then
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_COVERAGE=ON -DJAMSAT_DISABLE_OPTIMIZATIONS=ON ${TRAVIS_BUILD_DIR}
-  elif [ "${JAMSAT_MODE}" = "SANITIZERS" ]
-  then
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_SANITIZERS=ON -DJAMSAT_DISABLE_OPTIMIZATIONS=ON -DJAMSAT_USE_SEPARATE_CLANG_SANITIZERS=ON ${TRAVIS_BUILD_DIR}
-  else
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ${TRAVIS_BUILD_DIR}
-  fi
-
+build_and_test() {
   echo "Compilation database:"
   cat compile_commands.json
 
@@ -26,6 +13,32 @@ then
 
   echo "Testing..."
   ctest -V
+}
+
+if [ "${SONARSOURCE_SCAN}" != "1" ]
+then
+  echo "Building JamSAT in ${JAMSAT_MODE} mode."
+
+  if [ "${JAMSAT_MODE}" = "COVERAGE" ]
+  then
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_COVERAGE=ON -DJAMSAT_DISABLE_OPTIMIZATIONS=ON ${TRAVIS_BUILD_DIR}
+    build_and_test
+
+  elif [ "${JAMSAT_MODE}" = "SANITIZERS" ]
+  then
+    echo "Running address and leak sanitizer..."
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_SANITIZERS=ON -DJAMSAT_DISABLE_OPTIMIZATIONS=ON -DJAMSAT_USE_SEPARATE_CLANG_SANITIZERS=ON ${TRAVIS_BUILD_DIR}
+    build_and_test
+
+    mkdir ../build2
+    cd ../build2
+    echo "Running memory sanitizer..."
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_SANITIZERS=ON -DJAMSAT_DISABLE_OPTIMIZATIONS=ON -DJAMSAT_ENABLE_MEMORY_SANITIZER=ON ${TRAVIS_BUILD_DIR}
+    build_and_test
+  else
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ${TRAVIS_BUILD_DIR}
+    build_and_test
+  fi
 else
   cd ${TRAVIS_BUILD_DIR}
   cmake -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_COVERAGE=ON .
