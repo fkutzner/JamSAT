076fa62491afd6a1a2a8283ef970522e90c82eb9
Felix Kutzner <felixkutzner@gmail.com>
2017-07-04 19:54:43 +0200

    using code coverage mode for SonarSource analysis

--- etc/TravisBuild.sh
+++ etc/TravisBuild.sh
@@ -16,7 +16,7 @@ then
   ctest -V
 else
   cd ${TRAVIS_BUILD_DIR}
-  cmake -DCMAKE_BUILD_TYPE=Debug .
+  cmake -DCMAKE_BUILD_TYPE=Debug -DJAMSAT_ENABLE_COVERAGE=ON .
   build-wrapper-linux-x86-64 --out-dir bw-output make clean all
   ctest -V
   sonar-scanner
