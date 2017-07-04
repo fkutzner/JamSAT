e76dd50b9c7428281381b36a56862b2b4756f71a
Felix Kutzner <felixkutzner@gmail.com>
2017-07-04 18:35:15 +0200

    added boost header installation on Linux (Travis)

--- etc/TravisBuild.sh
+++ etc/TravisBuild.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+set -e
 
 # For this script, it's assumed that the current working directory is an empty
 # directory where the build files can be placed.
 
-cmake -DCMAKE_BUILD_TYPE=Debug ${TRAVIS_BUILD_DIR} || exit 1
-
-if [ "SONARSOURCE_SCAN" != "1" ]
+if [ "${TRAVIS_OS_NAME}" == "linux" ]
 then
-  cmake --build . -- -j2 || exit 1
-  ctest -V || exit 1
+  sudo apt-get install -y libboost-all-dev
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Debug ${TRAVIS_BUILD_DIR}
+
+if [ "${SONARSOURCE_SCAN}" != "1" ]
+then
+  cmake --build . -- -j2
+  ctest -V
 else
-  build-wrapper-linux-x86-64 --out-dir bw-output make clean all || exit 1
-  ctest -V || exit 1
-  sonar-scanner || exit 1
+  build-wrapper-linux-x86-64 --out-dir bw-output make clean all
+  ctest -V
+  sonar-scanner
 fi
