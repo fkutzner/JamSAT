e5afadb528d5ba8f5f5dc68a58045a93a0d36db5
Felix Kutzner <felixkutzner@gmail.com>
2018-01-02 23:04:45 +0100

    added a DRUP certificate implementation

---
+++ src/libjamsat/proof/PlainDRUPCertificate.cpp
@@ -0,0 +1,77 @@
+/* Copyright (c) 2017 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#include <libjamsat/proof/DRUPCertificate.h>
+
+namespace jamsat {
+
+class PlainDRUPCertificate : public DRUPCertificate {
+public:
+    PlainDRUPCertificate(std::ostream &outputStream);
+    virtual ~PlainDRUPCertificate();
+
+    PlainDRUPCertificate &operator=(const PlainDRUPCertificate &other) = delete;
+    PlainDRUPCertificate &operator=(PlainDRUPCertificate &&other) = delete;
+    PlainDRUPCertificate(const PlainDRUPCertificate &other) = delete;
+    PlainDRUPCertificate(PlainDRUPCertificate &&other) = delete;
+
+    void closeProof() override;
+
+protected:
+    void beginDeletedClause() override;
+    void addLiteral(CNFLit lit) override;
+    void endClause() override;
+
+private:
+    std::ostream &m_outputStream;
+};
+
+std::unique_ptr<DRUPCertificate> createPlainDRUPCertificate(std::ostream &outputStream) {
+    return std::make_unique<PlainDRUPCertificate>(outputStream);
+}
+
+PlainDRUPCertificate::PlainDRUPCertificate(std::ostream &outputStream)
+  : DRUPCertificate(), m_outputStream(outputStream) {}
+
+PlainDRUPCertificate::~PlainDRUPCertificate() {}
+
+void PlainDRUPCertificate::closeProof() {
+    m_outputStream << "0\n";
+    m_outputStream.flush();
+}
+
+void PlainDRUPCertificate::beginDeletedClause() {
+    m_outputStream << "d ";
+}
+
+void PlainDRUPCertificate::addLiteral(CNFLit lit) {
+    m_outputStream << lit << " ";
+}
+
+void PlainDRUPCertificate::endClause() {
+    m_outputStream << "0\n";
+}
+}
---
+++ testsrc/libjamsat/unit/proof/PlainDRUPCertificateUnitTests.cpp
@@ -0,0 +1,97 @@
+/* Copyright (c) 2017 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#include <gtest/gtest.h>
+
+#include <sstream>
+#include <vector>
+
+#include <libjamsat/cnfproblem/CNFLiteral.h>
+#include <libjamsat/proof/DRUPCertificate.h>
+
+namespace jamsat {
+TEST(UnitProof, PlainDRUPCertificate_trivialProofContainsEmptyClause) {
+    std::stringstream buffer;
+    auto underTest = createPlainDRUPCertificate(buffer);
+    underTest->closeProof();
+
+    std::string proof = buffer.str();
+    std::string expected = "0\n";
+    ASSERT_EQ(proof, expected);
+}
+
+TEST(UnitProof, PlainDRUPCertificate_singleRUPClauseWrittenToProof) {
+    std::stringstream buffer;
+    auto underTest = createPlainDRUPCertificate(buffer);
+    std::vector<CNFLit> clause{
+        CNFLit{CNFVar{0}, CNFSign::POSITIVE},
+        CNFLit{CNFVar{4}, CNFSign::NEGATIVE},
+        CNFLit{CNFVar{2}, CNFSign::NEGATIVE},
+    };
+    underTest->addRUPClause(clause);
+
+    std::string proof = buffer.str();
+    std::string expected = " 1 -5 -3 0\n";
+    ASSERT_EQ(proof, expected);
+}
+
+TEST(UnitProof, PlainDRUPCertificate_multipleRUPClausesWrittenToProof) {
+    std::stringstream buffer;
+    auto underTest = createPlainDRUPCertificate(buffer);
+    std::vector<CNFLit> clause1{
+        CNFLit{CNFVar{0}, CNFSign::POSITIVE},
+        CNFLit{CNFVar{4}, CNFSign::NEGATIVE},
+        CNFLit{CNFVar{2}, CNFSign::NEGATIVE},
+    };
+    underTest->addRUPClause(clause1);
+
+    std::vector<CNFLit> clause2{
+        CNFLit{CNFVar{1}, CNFSign::POSITIVE},
+        CNFLit{CNFVar{2}, CNFSign::NEGATIVE},
+        CNFLit{CNFVar{3}, CNFSign::NEGATIVE},
+    };
+    underTest->addRUPClause(clause2);
+
+    std::string proof = buffer.str();
+    std::string expected = " 1 -5 -3 0\n 2 -3 -4 0\n";
+    ASSERT_EQ(proof, expected);
+}
+
+TEST(UnitProof, PlainDRUPCertificate_deletedClauseWrittenToProof) {
+    std::stringstream buffer;
+    auto underTest = createPlainDRUPCertificate(buffer);
+    std::vector<CNFLit> clause{
+        CNFLit{CNFVar{0}, CNFSign::POSITIVE},
+        CNFLit{CNFVar{4}, CNFSign::NEGATIVE},
+        CNFLit{CNFVar{2}, CNFSign::NEGATIVE},
+    };
+    underTest->deleteClause(clause);
+
+    std::string proof = buffer.str();
+    std::string expected = "d  1 -5 -3 0\n";
+    ASSERT_EQ(proof, expected);
+}
+}
