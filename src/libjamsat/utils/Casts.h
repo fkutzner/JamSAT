117629d83274deda18f2244fe6dbafd40026c40e
Felix Kutzner <felixkutzner@gmail.com>
2018-01-04 20:20:45 +0100

    added a checked static cast

---
+++ src/libjamsat/utils/Casts.h
@@ -0,0 +1,66 @@
+/* Copyright (c) 2017 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#pragma once
+
+#if defined(JAM_ASSERT_ENABLED)
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include <libjamsat/utils/Assert.h>
+#endif
+
+namespace jamsat {
+/**
+ * \ingroup JamSAT_Utils
+ *
+ * \brief A static_cast with bounds checking when assertions are enabled.
+ *
+ * \param value     The value to be casted to \p ToType.
+ *
+ * \tparam ToType   The target integral type.
+ * \tparam FromType Any type that is integral or implicitly convertible to an integral type.
+ */
+template <typename ToType, typename FromType>
+auto static_checked_cast(FromType value) ->
+    typename std::enable_if<std::is_integral<ToType>::value, ToType>::type {
+#if defined(JAM_ASSERT_ENABLED)
+    if (value >= 0) {
+        uintmax_t i = static_cast<uintmax_t>(value);
+        uintmax_t max = static_cast<uintmax_t>(std::numeric_limits<ToType>::max());
+        JAM_ASSERT(i <= max, "Loss of precision caused loss of data");
+    } else {
+        intmax_t i = static_cast<intmax_t>(value);
+        intmax_t min = static_cast<intmax_t>(std::numeric_limits<ToType>::min());
+        JAM_ASSERT(std::numeric_limits<ToType>::min() < 0,
+                   "Cast of a negative integer to an unsigned type failed");
+        JAM_ASSERT(i >= min, "Loss of precision caused loss of data");
+    }
+#endif
+    return static_cast<ToType>(value);
+}
+}
---
+++ testsrc/libjamsat/unit/utils/CastsUnitTests.cpp
@@ -0,0 +1,122 @@
+/* Copyright (c) 2017 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <iostream>
+
+#include <libjamsat/utils/Assert.h>
+#include <libjamsat/utils/Casts.h>
+
+namespace jamsat {
+namespace {
+template <typename ToType, typename FromType>
+void test_staticCheckedCastSucceeds(FromType from) {
+#if defined(JAM_ASSERT_ENABLED)
+    static_checked_cast<ToType>(from);
+    // not expecting an exception to be thrown
+#else
+    (void)from std::cerr << "Warning: static cast checking is disabled, not testing anything"
+                         << std::endl;
+#endif
+}
+
+template <typename ToType, typename FromType>
+void test_staticCheckedCastFails(FromType from) {
+#if defined(JAM_ASSERT_ENABLED)
+    EXPECT_DEATH(static_checked_cast<ToType>(from), ".*");
+#else
+    (void)from std::cerr << "Warning: static cast checking is disabled, not testing anything"
+                         << std::endl;
+#endif
+}
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForSameWidthUintToUint) {
+    test_staticCheckedCastSucceeds<uint32_t>(uint32_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidSmallUintToLargeUint) {
+    test_staticCheckedCastSucceeds<uint64_t>(uint32_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidLargeUintToSmallUint) {
+    test_staticCheckedCastSucceeds<uint32_t>(uint64_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidLargeIntToSmallUint) {
+    test_staticCheckedCastSucceeds<uint32_t>(int64_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidSmallIntToLargeUint) {
+    test_staticCheckedCastSucceeds<uint64_t>(int32_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidSmallIntToSmallUint) {
+    test_staticCheckedCastSucceeds<uint32_t>(int32_t{4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidNegLargeIntToNegSmallInt) {
+    test_staticCheckedCastSucceeds<int32_t>(int64_t{-4});
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForValidNegSmallIntToNegLargeInt) {
+    test_staticCheckedCastSucceeds<int64_t>(int32_t{-4});
+}
+
+TEST(UnitUtils, staticCheckedCastFailsForInvalidUintToUintConversion) {
+    test_staticCheckedCastFails<uint16_t>(std::numeric_limits<uint32_t>::max());
+}
+
+TEST(UnitUtils, staticCheckedCastFailsForInvalidUintToSameWidthIntConversion) {
+    test_staticCheckedCastFails<int32_t>(std::numeric_limits<uint32_t>::max());
+}
+
+TEST(UnitUtils, staticCheckedCastFailsForInvalidNegIntToUintConversion) {
+    test_staticCheckedCastFails<uint32_t>(std::numeric_limits<int32_t>::min());
+}
+
+TEST(UnitUtils, staticCheckedCastFailsForInvalidNegIntToNegIntConversion) {
+    test_staticCheckedCastFails<int32_t>(std::numeric_limits<int64_t>::min());
+}
+
+namespace {
+class ImplicitlyConvertibleToInt {
+public:
+    ImplicitlyConvertibleToInt(int x) : m_x(x) {}
+
+    operator int() { return m_x; }
+
+private:
+    int m_x;
+};
+}
+
+TEST(UnitUtils, staticCheckedCastSucceedsForSameWidthImplicitConvIntToInt) {
+    test_staticCheckedCastSucceeds<int>(ImplicitlyConvertibleToInt{3});
+}
+}
