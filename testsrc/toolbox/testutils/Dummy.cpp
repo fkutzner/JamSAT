03d0c4e10f2f4fb4496179bf630bd02cab4ed494
Felix Kutzner <felixkutzner@gmail.com>
2018-01-04 15:00:08 +0100

    set up a testutils module

---
+++ testsrc/toolbox/testutils/Dummy.cpp
@@ -0,0 +1,5 @@
+// As long as this module is header-only, this file is needed to ensure that
+// CMake object library usage does not break IDE support.
+
+// Suppress warnings about empty files:
+char testutils_empty_file_dummy_sym;
