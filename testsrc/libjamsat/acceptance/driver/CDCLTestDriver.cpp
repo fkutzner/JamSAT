7fd0ba511113adb3cdb61ce7b99e2fc819ab183c
Felix Kutzner <felixkutzner@gmail.com>
2018-04-08 12:26:50 +0200

    add no-fail-on-parse-error mode to CDCLTestDriver

--- testsrc/libjamsat/acceptance/driver/CDCLTestDriver.cpp
+++ testsrc/libjamsat/acceptance/driver/CDCLTestDriver.cpp
@@ -7,13 +7,31 @@
 
 using SolverType = jamsat::CDCLSatSolver<>;
 
+namespace {
+void printUsage() {
+    std::cerr << "Usage: SmallRandomSATTestDriver (--fail-on-parse-error|--no-fail-on-parse-error) "
+                 "<FILENAME>\n";
+}
+
+int getParseErrorExitValue(std::string const &parseErrorMode) {
+    if (parseErrorMode == "--fail-on-parse-error") {
+        return EXIT_FAILURE;
+    }
+    if (parseErrorMode == "--no-fail-on-parse-error") {
+        return EXIT_SUCCESS;
+    }
+    std::cerr << "Unknown parse error failure mode " << parseErrorMode << std::endl;
+    return EXIT_FAILURE;
+}
+}
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: SmallRandomSATTestDriver <FILENAME>\n";
+    if (argc != 3) {
+        printUsage();
         return EXIT_FAILURE;
     }
 
-    std::ifstream cnfFile{argv[1]};
+    std::ifstream cnfFile{argv[2]};
     if (!cnfFile || !cnfFile.is_open()) {
         std::cerr << "Error: could not open file " << argv[1] << "\n";
         return EXIT_FAILURE;
@@ -23,11 +41,11 @@ int main(int argc, char **argv) {
     cnfFile >> problem;
     if (cnfFile.fail()) {
         std::cerr << "Error: could not parse " << argv[1] << "\n";
-        return EXIT_FAILURE;
+        return getParseErrorExitValue(argv[1]);
     }
 
     typename SolverType::Configuration config;
-    config.clauseMemoryLimit = 10495860;
+    config.clauseMemoryLimit = 10485760;
 
 
     SolverType solver{config};
