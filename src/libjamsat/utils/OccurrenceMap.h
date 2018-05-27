32c216f4ed2586c74b596d912e9cce984a8c9e7a
Felix Kutzner <felixkutzner@gmail.com>
2018-05-27 15:12:45 +0200

    add OccurrenceMap data type

---
+++ src/libjamsat/utils/OccurrenceMap.h
@@ -0,0 +1,165 @@
+/* Copyright (c) 2018 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#pragma once
+
+#include <libjamsat/utils/Assert.h>
+#include <libjamsat/utils/BoundedMap.h>
+
+#include <boost/range.hpp>
+#include <boost/range/algorithm_ext/erase.hpp>
+
+#include <cstdint>
+#include <vector>
+
+namespace jamsat {
+/**
+ * \class OccurrenceMap
+ *
+ * \brief A map-like type for keeping track of element occurrences in containers.
+ *
+ * \ingroup JamSAT_Utils
+ *
+ * \tparam Container                TODO: document this template parameter
+ * \tparam ContainerDeletedQuery    A type satisfying the STL UnaryPredicate concept for
+ *                                  `Container const*` arguments, with instances of
+ *                                  `ContainerDeletedQuery`indicating whether the container
+ *                                  has been marked for deletion.
+ * \tparam ContainerValueIndex      A `BoundedMap` index type for `Container::value_type`.
+ */
+template <typename Container, typename ContainerDeletedQuery,
+          typename ContainerValueIndex = typename Container::value_type::Index>
+class OccurrenceMap {
+public:
+    using ContainerRange = boost::iterator_range<typename std::vector<Container *const>::iterator>;
+
+    /**
+     * \brief Constructs an OccurrenceMap
+     *
+     * \param maxElement    The maximum element that may occur in the containers
+     *                      added to the OccurrenceMap.
+     */
+    OccurrenceMap(typename Container::value_type maxElement);
+
+    /**
+     * \brief Increases the maximum element that may occur in the containers added
+     *        to the OccurrenceMap
+     *
+     * \param maxElement    The maximum element that may occur in the containers
+     *                      added to the OccurrenceMap.
+     */
+    void increaseMaxElementTo(typename Container::value_type maxElement);
+
+    /**
+     * \brief Adds a container to the occurrence map.
+     *
+     * \param container     The container to be added.
+     */
+    void insert(Container &container);
+
+    /**
+     * \brief Returns a range of pointers to the containers in which \p index occurs.
+     *
+     * \param value         A value not greater than the current maximum element.
+     * \returns             A range of pointers to the containers in which \p index
+     *                      occurs.
+     */
+    auto operator[](typename Container::value_type value) noexcept -> ContainerRange;
+
+    /**
+     * \brief Marks a container as to-be-deleted from the occurrence map.
+     *
+     * Unless elements have been removed from \p container, \p container will not be
+     * returned in future calls to `operator[]()`.
+     *
+     * Precondition: \p container must be marked as to-be-deleted by all
+     * `ContainerDeletedQuery` objects.
+     *
+     * \param container     The container to be marked as to-be-deleted from the
+     *                      occurrence map.
+     */
+    void remove(Container const &container) noexcept;
+
+    OccurrenceMap(OccurrenceMap const &rhs) = delete;
+    auto operator=(OccurrenceMap const &rhs) -> OccurrenceMap & = delete;
+
+    OccurrenceMap(OccurrenceMap &&rhs) noexcept = default;
+    auto operator=(OccurrenceMap &&rhs) noexcept -> OccurrenceMap & = default;
+
+private:
+    using value_type = typename Container::value_type;
+    struct OccurrenceListWithFlags {
+        bool m_requiresUpdate;
+        std::vector<Container *> m_occList;
+    };
+
+    BoundedMap<typename Container::value_type, OccurrenceListWithFlags, ContainerValueIndex>
+        m_occurrences;
+    ContainerDeletedQuery m_deletedQuery;
+};
+
+/********** Implementation ****************************** */
+
+template <typename Container, typename ContainerDeletedQuery, typename ContainerValueIndex>
+OccurrenceMap<Container, ContainerDeletedQuery, ContainerValueIndex>::OccurrenceMap(
+    typename Container::value_type maxElement)
+  : m_occurrences(maxElement), m_deletedQuery() {}
+
+template <typename Container, typename ContainerDeletedQuery, typename ContainerValueIndex>
+void OccurrenceMap<Container, ContainerDeletedQuery, ContainerValueIndex>::increaseMaxElementTo(
+    typename Container::value_type maxElement) {
+    m_occurrences.increaseSizeTo(maxElement);
+}
+
+template <typename Container, typename ContainerDeletedQuery, typename ContainerValueIndex>
+void OccurrenceMap<Container, ContainerDeletedQuery, ContainerValueIndex>::insert(
+    Container &container) {
+    for (auto &element : container) {
+        m_occurrences[element].m_occList.push_back(&container);
+    }
+}
+
+template <typename Container, typename ContainerDeletedQuery, typename ContainerValueIndex>
+void OccurrenceMap<Container, ContainerDeletedQuery, ContainerValueIndex>::remove(
+    Container const &container) noexcept {
+    JAM_ASSERT(m_deletedQuery(&container),
+               "Only remove containers marked for deletion may be deleted");
+    for (auto &element : container) {
+        m_occurrences[element].m_requiresUpdate = true;
+    }
+}
+
+template <typename Container, typename ContainerDeletedQuery, typename ContainerValueIndex>
+auto OccurrenceMap<Container, ContainerDeletedQuery, ContainerValueIndex>::
+operator[](typename Container::value_type value) noexcept -> ContainerRange {
+    auto &occList = m_occurrences[value].m_occList;
+    if (m_occurrences[value].m_requiresUpdate) {
+        boost::remove_erase_if(occList, m_deletedQuery);
+        m_occurrences[value].m_requiresUpdate = false;
+    }
+    return boost::make_iterator_range(occList.cbegin(), occList.cend());
+}
+}
---
+++ testsrc/libjamsat/unit/utils/OccurrenceMapTests.cpp
@@ -0,0 +1,126 @@
+/* Copyright (c) 2018 Felix Kutzner (github.com/fkutzner)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ Except as contained in this notice, the name(s) of the above copyright holders
+ shall not be used in advertising or otherwise to promote the sale, use or
+ other dealings in this Software without prior written authorization.
+
+*/
+
+#include <gtest/gtest.h>
+
+#include <libjamsat/utils/OccurrenceMap.h>
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+namespace jamsat {
+namespace {
+struct TestUIntIndex {
+    static size_t getIndex(uint32_t item) { return item; }
+};
+
+class TestUIntVec : public std::vector<uint32_t> {
+public:
+    TestUIntVec() : std::vector<uint32_t>{}, m_deleteFlag{false} {}
+    TestUIntVec(std::initializer_list<uint32_t> values)
+      : std::vector<uint32_t>{values}, m_deleteFlag{false} {}
+
+    void setDeleted() noexcept { m_deleteFlag = true; }
+
+    bool isDeleted() const noexcept { return m_deleteFlag; }
+
+
+private:
+    bool m_deleteFlag;
+};
+
+class TestUIntVecDelPred {
+public:
+    bool operator()(TestUIntVec const *x) const noexcept { return x->isDeleted(); }
+};
+}
+
+TEST(UnitUtils, emptyOccurrenceMapContainsNoEntries) {
+    jamsat::OccurrenceMap<TestUIntVec, TestUIntVecDelPred, TestUIntIndex> underTest{10};
+    for (uint32_t i = 0; i <= 10; ++i) {
+        EXPECT_TRUE(underTest[i].empty());
+    }
+}
+
+TEST(UnitUtils, elementsAreRetrievableFromOccurrenceMap) {
+    jamsat::OccurrenceMap<TestUIntVec, TestUIntVecDelPred, TestUIntIndex> underTest{31};
+    TestUIntVec testData1{9, 10, 15};
+    TestUIntVec testData2{22, 10, 13};
+
+    std::vector<std::vector<TestUIntVec *>> expected;
+    expected.resize(32);
+    expected[9].push_back(&testData1);
+    expected[10].push_back(&testData1);
+    expected[10].push_back(&testData2);
+    expected[13].push_back(&testData2);
+    expected[15].push_back(&testData1);
+    expected[22].push_back(&testData2);
+
+    underTest.insert(testData1);
+    underTest.insert(testData2);
+
+    for (uint32_t i = 0; i < 32; ++i) {
+        ASSERT_EQ(underTest[i].size(), expected[i].size())
+            << "Expected elements mismatch at index " << i;
+        auto occurrences = underTest[i];
+        EXPECT_TRUE(
+            std::is_permutation(occurrences.begin(), occurrences.end(), expected[i].begin()))
+            << "Expected elements mismatch at index " << i;
+    }
+}
+
+TEST(UnitUtils, deletedElementsAreRemovedFromOccurrenceMap) {
+    jamsat::OccurrenceMap<TestUIntVec, TestUIntVecDelPred, TestUIntIndex> underTest{31};
+    TestUIntVec testData1{9, 10, 15};
+    TestUIntVec testData2{22, 10, 13};
+    TestUIntVec testData3{22, 10};
+
+    std::vector<std::vector<TestUIntVec *>> expected;
+    expected.resize(32);
+    expected[10].push_back(&testData2);
+    expected[10].push_back(&testData3);
+    expected[13].push_back(&testData2);
+    expected[22].push_back(&testData2);
+    expected[22].push_back(&testData3);
+
+    underTest.insert(testData1);
+    underTest.insert(testData2);
+    underTest.insert(testData3);
+
+    testData1.setDeleted();
+    underTest.remove(testData1);
+
+    for (uint32_t i = 0; i < 32; ++i) {
+        ASSERT_EQ(underTest[i].size(), expected[i].size())
+            << "Expected elements mismatch at index " << i;
+        auto occurrences = underTest[i];
+        EXPECT_TRUE(
+            std::is_permutation(occurrences.begin(), occurrences.end(), expected[i].begin()))
+            << "Expected elements mismatch at index " << i;
+    }
+}
+}
